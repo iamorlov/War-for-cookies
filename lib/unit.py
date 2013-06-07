4ada9771c1b4e24dcb96c42d68afafce0b95e42c
KIO <jeo2145@gmail.com>
2013-06-07 23:24:41 +0300

    Добавлин прототип юнитов, только начало

---
+++ lib/unit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time, pygame 
+
+class unit():
+        id_unit=''
+        name=''
+        hit_point
+        cost_co
+        cost_milk
+        kick
+        rang
+     def set_un(self):
+
+if __name__ == '__main__':
+     Unit=unit()
+     Unit.artillery()    
