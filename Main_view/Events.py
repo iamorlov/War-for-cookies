77505bcb71a0b7d773f2700277867d7823ce6cd1
azon1272 <azon1272@gmail.com>
2013-06-16 01:24:34 +0300

    Small update: created class "Events" from class "Window", but  methods that duplicate do not removed.

--- Main_view/Events.py
+++ Main_view/Events.py
@@ -4,57 +4,40 @@ Created on 15 черв. 2013
 
 @author: antimoskal
 '''
+from pygame import *
+import sys, Window
 
-import pygame, sys, Window
-#from Window import *
-
-class Events(Window):
+class Events():
   
     def __init__(self):
 #        self.window = Window()
         pass
         
-    def get_event(self):#,flag):
+    def get_event(self,big_step,step_p):#,flag):
         for self.event in pygame.event.get():
             if self.event.type == MOUSEBUTTONDOWN:
                 if self.event.button == 1:
                     click_coords = self.event.pos
-                    self.Get_map_coords(click_coords)
-                    self.Get_minimap_coords(click_coords)
+                    self.Get_map_coords(click_coords,big_step)
+                    self.Get_minimap_coords(click_coords,step_p)
             if self.event.type == QUIT:
                 sys.exit()
             if self.event.type == KEYDOWN:
                 if self.event.key == K_F5:
                     self.event_scan_keyes()
     
-    def get_map_coords(self,click_coords):
-        self.Load_part_of_map()
-        #self.window.Load_part_of_map()            
+    def get_map_coords(self,click_coords,step):        
         if ((click_coords[0] > 20) and (click_coords[1] > 0) and (click_coords[0] < 720) and (click_coords[1] < 700)):
-            self.x_coord = (click_coords[0]-20)//self.big_step
-            self.y_coord = (click_coords[1])//self.big_step
-#            self.change_cell(self.y_coord+self.y_coord_start,self.x_coord+self.x_coord_start,  self.cell_type, 0, 0)
-            
-#            first_texture = self.textures[self.cell_type].get_rect()
-#            first_texture.center=(35+self.x_coord*self.big_step,14+self.y_coord*self.big_step)
-#            self.display.blit(self.textures[self.cell_type],first_texture)   
-            cell = Rect((self.x_coord*self.big_step+20, self.y_coord*self.big_step),(self.big_step,self.big_step))
-
-#            print self.cell_type
-            pygame.draw.rect(self.display,(0,0,0),cell,2)
-            #self.window.Maps_grid()
-            self.Maps_grid()
-            pygame.display.flip()
+            x_coord = (click_coords[0]-20)//step
+            y_coord = (click_coords[1])//step
+            return x_coord, y_coord
+        else:
+            return 0, 0
     
-    def get_minimap_coords(self,click_coords):
+    def get_minimap_coords(self,click_coords,step):
         if ((click_coords[0] > 800) and (click_coords[1] > 0) and (click_coords[0] < 1100) and (click_coords[1] < 300)):
-            self.minimap_x = (click_coords[0]-800)//self.step_p
-            self.minimap_y = (click_coords[1])//self.step_p
-            self.window.Load_part_of_map()
-            cell = Rect((800+self.x_coord_start*self.step_p,0+self.y_coord_start*self.step_p),(self.step_p*14,self.step_p*14))
-            self.load_cells_list()
-            self.Minimap()
-            pygame.draw.rect(self.display,(0,0,0),cell,2)
-            #self.window.Maps_grid()
-            self.Maps_grid()         
-###
+            minimap_x = (click_coords[0]-800)//step
+            minimap_y = (click_coords[1])//step
+            return minimap_x, minimap_y
+        else:
+            return 0, 0
