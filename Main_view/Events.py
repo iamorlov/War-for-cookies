8b8b9d34173bb5cc579ef1609ad743455e20852f
azon1272 <azon1272@gmail.com>
2013-06-16 11:37:09 +0300

    Small update: fixed class "Events".

--- Main_view/Events.py
+++ Main_view/Events.py
@@ -4,40 +4,44 @@ Created on 15 черв. 2013
 
 @author: antimoskal
 '''
-from pygame import *
+import pygame
 import sys, Window
 
 class Events():
   
     def __init__(self):
-#        self.window = Window()
+        pygame.init()
         pass
-        
-    def get_event(self,big_step,step_p):#,flag):
-        for self.event in pygame.event.get():
-            if self.event.type == MOUSEBUTTONDOWN:
-                if self.event.button == 1:
-                    click_coords = self.event.pos
-                    self.Get_map_coords(click_coords,big_step)
-                    self.Get_minimap_coords(click_coords,step_p)
-            if self.event.type == QUIT:
-                sys.exit()
-            if self.event.type == KEYDOWN:
-                if self.event.key == K_F5:
-                    self.event_scan_keyes()
     
     def get_map_coords(self,click_coords,step):        
-        if ((click_coords[0] > 20) and (click_coords[1] > 0) and (click_coords[0] < 720) and (click_coords[1] < 700)):
             x_coord = (click_coords[0]-20)//step
             y_coord = (click_coords[1])//step
             return x_coord, y_coord
-        else:
-            return 0, 0
     
     def get_minimap_coords(self,click_coords,step):
-        if ((click_coords[0] > 800) and (click_coords[1] > 0) and (click_coords[0] < 1100) and (click_coords[1] < 300)):
             minimap_x = (click_coords[0]-800)//step
             minimap_y = (click_coords[1])//step
             return minimap_x, minimap_y
+    
+    def mousebuttondown_terms(self,click_coords,big_step,step_p):
+        if ((click_coords[0] > 20) and (click_coords[1] > 0) and (click_coords[0] < 720) and (click_coords[1] < 700)):        
+            x, y = self.get_map_coords(click_coords,big_step)
+            return ['map_coords', x, y]
+        elif ((click_coords[0] > 800) and (click_coords[1] > 0) and (click_coords[0] < 1100) and (click_coords[1] < 300)):
+            x, y = self.get_minimap_coords(click_coords,step_p)
+            return ['minimap_coords',x,y]
         else:
-            return 0, 0
+            return None
+        
+    def get_event(self,big_step,step_p):#,flag):
+        for self.event in pygame.event.get():
+            if self.event.type == pygame.MOUSEBUTTONDOWN:
+                if self.event.button == 1:
+                    click_coords = self.event.pos
+                    return self.mousebuttondown_terms(click_coords, big_step, step_p)
+            if self.event.type == pygame.QUIT:
+                sys.exit()
+            if self.event.type == pygame.KEYDOWN:
+                if self.event.key == pygame.K_F5:
+                    pass
+                    #self.event_scan_keyes()
--- Main_view/Window.py
+++ Main_view/Window.py
@@ -4,7 +4,7 @@ from Core import *
 from ResManager import *
 from Resources import *
 from Graphical_logic import *
-#from Events import *
+from Events import *
 
 class Window(Core):
      
@@ -12,7 +12,7 @@ class Window(Core):
         pygame.init()
         self.resources = Resources()
         self.graphical_logic = Graphical_logic()
-#        self.w_event = Events()
+        self.w_event = Events()
         print 'lol'
         self.minimap_x =0
         self.minimap_y =0
@@ -67,108 +67,62 @@ class Window(Core):
     def moving_army(self):
         pass
            
-    def Get_minimap_coords(self,click_coords):                 
-        if ((click_coords[0] > 800) and (click_coords[1] > 0) and (click_coords[0] < 1100) and (click_coords[1] < 300)):
-            self.minimap_x = (click_coords[0]-800)//self.step_p
-            self.minimap_y = (click_coords[1])//self.step_p
-            self.Load_part_of_map()
-            cell = Rect((800+self.x_coord_start*self.step_p,0+self.y_coord_start*self.step_p),(self.step_p*14,self.step_p*14))
-            self.load_cells_list()
-            self.Minimap()
-            pygame.draw.rect(self.display,(0,0,0),cell,2)
-            self.Maps_grid()
-            #self.Minimap()
-
-    def Get_map_coords(self,click_coords):
-        self.Load_part_of_map()                
-        if ((click_coords[0] > 20) and (click_coords[1] > 0) and (click_coords[0] < 720) and (click_coords[1] < 700)):
-            self.x_coord = (click_coords[0]-20)//self.big_step
-            self.y_coord = (click_coords[1])//self.big_step
-#            self.change_cell(self.y_coord+self.y_coord_start,self.x_coord+self.x_coord_start,  self.cell_type, 0, 0)
-            
-#            first_texture = self.textures[self.cell_type].get_rect()
-#            first_texture.center=(35+self.x_coord*self.big_step,14+self.y_coord*self.big_step)
-#            self.display.blit(self.textures[self.cell_type],first_texture)   
-            cell = Rect((self.x_coord*self.big_step+20, self.y_coord*self.big_step),(self.big_step,self.big_step))
-
-#            print self.cell_type
-            pygame.draw.rect(self.display,(0,0,0),cell,2)
-            self.Maps_grid()
-            pygame.display.flip()
-
-    def event_scan_keyes(self):
-        pygame.event.clear()
-        self.window_for_save()
-        for event in pygame.event.get():
-            print type(event)
-            print event.type
-            if event.type == KEYDOWN:
-                print event.type
-                print type(event.key)
-                name_key =(re.search('[a-z]',pygame.key.name(event.key)))
-                if name_key != None:
-                    text = (name_key).group(0)
-                    print text
-                    return text
-                else:
-                    return ''
-            else:
-                print 'lol'
-                return 'lol'
-        pygame.display.update()
-        pygame.event.clear()                      
+#    def event_scan_keyes(self):
+#        pygame.event.clear()
+#        self.window_for_save()
+#        for event in pygame.event.get():
+#            print type(event)
+#            print event.type
+#            if event.type == KEYDOWN:
+#                print event.type
+#                print type(event.key)
+#                name_key =(re.search('[a-z]',pygame.key.name(event.key)))
+#                if name_key != None:
+#                    text = (name_key).group(0)
+#                    print text
+#                    return text
+#                else:
+#                    return ''
+#            else:
+#                print 'lol'
+#                return 'lol'
+#        pygame.display.update()
+#        pygame.event.clear()                      
     
-    def window_for_save(self):
-        pygame.event.clear()
-        cell = Rect((360,260),(300,200))
-        pygame.draw.rect(self.display,(204,204,204),cell,0)
-        cell = Rect((385,280),(250,50))
-        pygame.draw.rect(self.display,(255,255,204),cell,0)
-        pygame.draw.rect(self.display,(0,0,0),cell,2)
-        #text = pygame.font.SysFont(name, size, bold, italic)
-        font1 = pygame.font.SysFont("Monospace", 20, bold=True, italic=False)
-        font2 = pygame.font.SysFont("Monospace", 20, bold=True, italic=False)        
-        item = u'Press enter for save'
-        item2 = u'Press ESC for exit'
-        font1 = font1.render(item,0,(20,20,20))
-        self.display.blit(font1,(385,360))
-        font2 = font2.render(item2,0,(20,20,20))
-        self.display.blit(font2,(385,410))
-        text = ''
-        pygame.display.update()
+#    def window_for_save(self):
+#        pygame.event.clear()
+#        cell = Rect((360,260),(300,200))
+#        pygame.draw.rect(self.display,(204,204,204),cell,0)
+#        cell = Rect((385,280),(250,50))
+#        pygame.draw.rect(self.display,(255,255,204),cell,0)
+#        pygame.draw.rect(self.display,(0,0,0),cell,2)
+#        #text = pygame.font.SysFont(name, size, bold, italic)
+#        font1 = pygame.font.SysFont("Monospace", 20, bold=True, italic=False)
+#        font2 = pygame.font.SysFont("Monospace", 20, bold=True, italic=False)        
+#        item = u'Press enter for save'
+#        item2 = u'Press ESC for exit'
+#        font1 = font1.render(item,0,(20,20,20))
+#        self.display.blit(font1,(385,360))
+#        font2 = font2.render(item2,0,(20,20,20))
+#        self.display.blit(font2,(385,410))
+#        text = ''
+#        pygame.display.update()
 
-
-    def Reload_window(self):
-        self.Maps_grid()
-        self.Minimaps_grid()
-        self.Minimap()
-        self.Type_of_grids()
-        pygame.display.flip()
+#    def Reload_window(self):
+#        self.Maps_grid()
+#        self.Minimaps_grid()
+#        self.Minimap()
+#        self.Type_of_grids()
+#        pygame.display.flip()
                 
 #    def Save_map(self,event):
 #        if event.type == KEYDOWN:
 #            if event.key == K_F5:
 #                self.       
 
-
-# Система подій підлягає повній переробці, потрібно вводити flags для нормального функціонування і відслідковування іншихз подій.
-    
-    def Events(self):
-        for self.event in pygame.event.get():
-            if self.event.type == MOUSEBUTTONDOWN:
-                if self.event.button == 1:
-                    click_coords = self.event.pos
-                    self.Get_map_coords(click_coords)
-                    self.Get_minimap_coords(click_coords)
-            if self.event.type == QUIT:
-                sys.exit()
-            if self.event.type == KEYDOWN:
-                if self.event.key == K_F5:
-                    self.event_scan_keyes()
-                    
-    def Load_part_of_map(self):
+    def Load_part_of_map(self,x,y):
         textures = self.resources.textures()
-        cells_list = self.load_cells(self.minimap_x, self.minimap_y)
+        cells_list = self.load_cells(x, y)
         for i in range(self.big_steps):
             for j in range(self.big_steps):
                 cell_type = cells_list[i*self.big_steps+j][2]
@@ -194,10 +148,35 @@ class Window(Core):
         pygame.draw.rect(self.display,(0,0,0),cell,2)
         self.Maps_grid()
         pygame.display.flip()
+
+    def event_handler(self):
+        event = self.w_event.get_event(self.big_step, self.step_p)
+        if (event != None):
+            if (event[0]=='map_coords'):
+                print event
+                self.action_to_map_coords(event[1],event[2])
+            elif (event[0]=='minimap_coords'):
+                print event
+                self.action_to_minimap_coords(event[1],event[2])
+
+    def action_to_map_coords(self,x,y):
+#        self.Load_part_of_map(x,y)                
+        cell = Rect((x*self.big_step+20, y*self.big_step),(self.big_step,self.big_step))
+        pygame.draw.rect(self.display,(0,0,0),cell,2)
+        self.Maps_grid()
+        pygame.display.flip()        
+
+    def action_to_minimap_coords(self,x,y):
+        self.Load_part_of_map(x,y)
+        cell = Rect((800+self.x_coord_start*self.step_p,0+self.y_coord_start*self.step_p),(self.step_p*14,self.step_p*14))
+        self.load_cells_list()
+        self.Minimap()
+        pygame.draw.rect(self.display,(0,0,0),cell,2)
+        self.Maps_grid()
+            #self.Minimap()        
 # 28x28, 25x25 cells
     def Rewrite_cell(self):
-        #self.w_event.get_event()#(self.flag)
-        self.Events()
+        self.event_handler()
 
     def Run(self):
         self.Main_Window()
@@ -205,7 +184,7 @@ class Window(Core):
         self.Maps_grid()
         self.Minimaps_grid()
         self.Minimap()
-        self.Load_part_of_map()
+        self.Load_part_of_map(0,0)
         while True:
             self.Rewrite_cell()
 
